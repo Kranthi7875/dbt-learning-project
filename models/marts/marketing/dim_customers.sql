26a57709c4623b93e8faf5c618f81cc80d9eeb42
Kranthi Vantaku <vantaku.kranthi7875@gmail.com>
2026-02-05 20:41:55 +0000

    Finalize dbt models for Jaffle Shop exercise

---
+++ models/marts/marketing/dim_customers.sql
@@ -0,0 +1,50 @@
+with customers as (
+
+    select * from {{ ref('stg_jaffle_shop__customers') }}
+
+),
+
+orders as (
+
+    select * from {{ ref('stg_jaffle_shop__orders') }}
+
+),
+
+customer_orders as (
+
+    select
+        customer_id,
+        min(order_date) as first_order_date,
+        max(order_date) as most_recent_order_date,
+        count(order_id) as number_of_orders
+    from orders
+    group by 1
+
+),
+
+customer_payments as (
+
+    select
+        customer_id,
+        sum(amount) as lifetime_value
+    from {{ ref('fct_orders') }}
+    group by 1
+
+),
+
+final as (
+
+    select
+        c.customer_id,
+        c.first_name,
+        c.last_name,
+        co.first_order_date,
+        co.most_recent_order_date,
+        coalesce(co.number_of_orders, 0) as number_of_orders,
+        coalesce(cp.lifetime_value, 0) as lifetime_value
+    from customers c
+    left join customer_orders co using (customer_id)
+    left join customer_payments cp using (customer_id) order by customer_id
+)
+
+select * from final
--- models/playground.sql
+++
@@ -1,8 +0,0 @@
-{{ config(materialized='view') }}
-
-select
-  year,
-  count(*) as births
-from `bigquery-public-data.samples.natality`
-group by year
-order by year
